55a144317720c7c32f44ac878cb4da75d7b651ae
Ayesha Ansari <ayesha.x.ansari@gmail.com>
2019-01-31 21:37:36 +0000

    Automatically backed up by Learn

--- hello-world.js
+++ hello-world.js
@@ -1,12 +1,11 @@
-//var hello = function(){
-//   return  "Hello World" }
-
-//function hello(){
-//  return "Hello World"
-//}
-
-hello(){
+var hello = function(){
+   return  "Hello World"
+}
+/*
+Another way to define methode
+function helloWorld(){
   return "Hello World"
 }
+*/
 
 module.exports = hello
